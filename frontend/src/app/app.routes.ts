b7cd2521eef46c08035ae1aabb9f56eeda64943b
afethi <fethiabderrahmane1@gmail.com>
2025-11-25 16:34:43 +0100

    feat: add routing for home, login, and register pages
    - Implemented routing in app.routes.ts to navigate between Home, Login, and Register components.
    - Added redirection from the root path to the home page.
    
    style: update app component to include Material Icons
    
    - Imported MatIconModule in app.ts to enable the use of Material Icons throughout the application.
    
    style: include Google Fonts and Material Icons in index.html
    
    - Added links to Google Fonts for Roboto and Material Icons in index.html for consistent typography and icon usage.
    
    feat: create home page layout with navigation and feed
    
    - Developed home.html and home.css to structure the main layout, including a top navigation bar, sidebars, and a feed section.
    - Implemented responsive design adjustments for tablet and mobile views.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Register } from './pages/register/register';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: Home
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'register',
+        component: Register
+    }
+];
---
+++ frontend/src/app/pages/home/home.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
